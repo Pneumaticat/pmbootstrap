ae4c3d38369587fec72f86dd67829e4221087183
Yuval Adam <_@yuv.al>
2017-08-20 11:48:06 +0300

    Fix firmware loading scripts per review

--- aports/main/postmarketos-base/firmware.sh
+++ aports/main/postmarketos-base/firmware.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
-# udev dynamic firmware loading script
-########################################################################
 
 FIRMWARE_DIRS="/lib/firmware /lib/firmware/postmarketos"
 
-exec >>/root/firmwareload.log 2>&1
+exec >>/var/log/firmwareload.log 2>&1
 
 if [ ! -e /sys/$DEVPATH/loading ]; then
-    echo "firmware loader misses sysfs directory"
-    exit 0
+    echo "Firmware loader misses sysfs directory"
+    exit 1
 fi
 
 for DIR in $FIRMWARE_DIRS; do
     [ -e "$DIR/$FIRMWARE" ] || continue
-    echo "loading $DIR/$FIRMWARE"
+    echo "Loading $DIR/$FIRMWARE"
     echo 1 > /sys/$DEVPATH/loading
     cat "$DIR/$FIRMWARE" > /sys/$DEVPATH/data
     echo 0 > /sys/$DEVPATH/loading
@@ -21,5 +19,5 @@ for DIR in $FIRMWARE_DIRS; do
 done
 
 echo -1 > /sys/$DEVPATH/loading
-echo "Cannot find  firmware file '$FIRMWARE'"
+echo "Cannot find firmware file '$FIRMWARE'"
 exit 1
